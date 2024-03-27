e5224f046767c6252c562fadca5c26de5993b493
eihaku <eihaku0205@gmail.com>
2024-03-27 10:10:00 +0900

    新規作成

---
+++ Test001/Test001.cpp
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include "Test001.h"
+
+int main()
+{
+    int year;
+    int weight,speed;
+    int money;
+    int average;
+    int sum;
+    int a, b,c,d;
+    int eki[10];
+    //scanf("%d", &year);
+
+    ////fun2_2(year);
+    //printf("重さを入力してください\n");
+    //scanf("%d", &weight);
+    //printf("速達しますか１はします、０はしません\n");
+    //scanf("%d", &speed);
+    //money = fun2_4(weight,speed);
+
+    //average=fun3_1();
+    //printf("%d", average);
+
+    //fun3_2();
+
+    //printf("整数を入力してください");
+    //scanf("%d", &arr);
+    //sum = fun3_4(arr);
+    //printf("%d", sum);
+
+
+    //printf("输入数字1 :");
+    //scanf("%d", &a);
+    //printf("输入数字2 :");
+    //scanf("%d", &b);
+    //c =fun3_7(a, b);
+    //d = fun3_7_1(a, b);
+    //printf("%d\n", c);
+    //printf("%d\n", d);
+
+    //return 0;
+
+    //printf("输入正方形边长：");
+    //scanf("%d", &a);
+    //fun4_1(a);
+
+    //{
+    //    char arr[] = {3,5,6,7,89,3,2,3,2,2};
+    //    a = sizeof(arr);
+    //    fun4_2(arr,a);
+    //}
+
+    //{
+    //    fun4_4(10);
+    //}
+
+    //{
+    //    fun4_5(15);
+    //}
+
+    //fun6_1();
+
+    fun6_2(17);
+
+
+}
+
---
+++ Test001/Test001.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef _TEST_H
+#define _TEST_H
+
+#pragma warning (disable:4996)
+
+
+int fun2_2(int year);
+int fun2_4(int weight, int speed);
+int fun3_1();
+int fun3_2();
+int fun3_4(int arr);
+int fun3_7(int a, int b);
+int fun3_7_1(int a, int b);
+int fun3_8(int eki);
+int fun4_1(int a);
+int fun4_2(char arr[],int a);
+int fun4_4(int a);
+int fun4_5(int a);
+int fun6_1();
+int fun6_2(char b);
+
+#endif // !_TEST_H
