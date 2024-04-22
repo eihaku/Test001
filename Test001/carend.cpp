bd96610d45c3a0bd0b7da77a87931c8ea2f1b1e1
eihaku <eihaku0205@gmail.com>
2024-04-22 10:40:09 +0900

    fun6_3_2()機能更新

--- Test001/carend.cpp
+++ Test001/carend.cpp
@@ -6,8 +6,9 @@
 
 int fun6_3_2(int year,int month)
 {
-    int day;
+    int day,temp,i,num;
     int lastday[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    char week[][7] = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
 
         if (month > 12 ||month < 0)
         {
@@ -21,10 +22,56 @@ int fun6_3_2(int year,int month)
         else {
             day = lastday[month - 1] + runyear(year);
         }
-        printf("num = %d\n", day);
+        printf("num = %d\n", day);     //月に数日ある
+        temp = WhatDay(year, month, 1);//月一日は何曜日
+        num = 7 - temp;                //土曜日の計算
+        for (i = 0; i <= 6; i++)       //日から土を表示
+        {
+            printf("  %s", week[i]);
+        }
+        printf("\n");
+        for (i = 0; i < temp; i++)     //一日は日曜日ではない場合スペース
+        {
+            printf("     ");
+        }
+        for (i = 1; i <= day; i++)     
+        {
+            if (i % num == 0)
+            {
+                printf("%4d\n", i);    //土曜日になったら\n
+                num = num + 7;         //次週の土曜日を計算
+            }
+            else
+            {
+                printf("%4d ", i);
+            }
+        }
+
+
     return 0;
 }
-
+int WhatDay(int y, int m, int d)
+{
+    int a;
+    if (m == 1 || m == 2)
+    {
+        m += 12;
+        y--;
+    }
+    int iWeek = (d + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7;
+    switch (iWeek)
+    {
+    case 0:  a = 1; break;
+    case 1:  a = 2; break;
+    case 2:  a = 3; break;
+    case 3:  a = 4; break;
+    case 4:  a = 5; break;
+    case 5:  a = 6; break;
+    case 6:  a = 7; break;
+    }
+    printf("%d", a);
+    return a;
+}
 int fun6_3_1(int year, int month)
 {
     int temp1 = year;
--- Test001/carend.h
+++ Test001/carend.h
@@ -7,5 +7,6 @@ int getdays(int month, int year);
 int runyear(int year);
 
 int fun6_3_2(int year, int month);
+int WhatDay(int y, int m, int d);
 
 #endif // !_TEST_H
